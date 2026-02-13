bc89aa9248facc01fb26f98b930db27c30c63553
Niccolò.Messina <niccolomessi25@gmail.com>
2026-02-13 10:19:40 +0100

    Initial commit

---
+++ src/main/java/com/example/progettoW6D5/ProgettoW6D5Application.java
@@ -0,0 +1,13 @@
+package com.example.progettoW6D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoW6D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoW6D5Application.class, args);
+	}
+
+}
